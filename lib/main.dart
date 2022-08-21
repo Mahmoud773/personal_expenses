bba93e3a22956342a029ece3981ae88a2a7dc492
Mahmoud Sadek <mahmoud.mostafa.sadek@gmail.com>
2022-08-21 20:43:43 +0200

    second task

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:personal_expenses/widgets/transaction_list.dart';
 import 'models/transaction.dart';
 
 void main() {
+  // second task
 // task for test only
   runApp(const MyApp());
 }
