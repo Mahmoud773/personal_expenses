4d60dc583613b99e171ba2b2c7fa17e5ad71bc58
Mahmoud Sadek <mahmoud.mostafa.sadek@gmail.com>
2022-08-21 19:55:14 +0200

    new task for test

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,8 @@ import 'package:personal_expenses/widgets/transaction_list.dart';
 import 'models/transaction.dart';
 
 void main() {
-// to check commit pleassesssssssssss
-// first commit
-runApp(const MyApp());
+// task for test only
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
